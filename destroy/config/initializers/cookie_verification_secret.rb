ad2f026e544a6a94a9a0d48dc9e3f952192ae486
Jon Lochhaas <jon@oibrilabs.com>
2013-09-17 13:52:43 -0400

    Finish layout and routes

--- destroy/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '00acf6b13e54f1c7f4a2d989ea33294df2d9e54ea0405750006a41d24fbd1525d80987fbddf2f887c4f33896087ecae8eeb8c2485535da691c632595aba44cb7';
